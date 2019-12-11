6259bb4b5cbad983a4cd3d994c9d1be71729e766
Will Harris <will.t.harris@protonmail.com>
2019-12-10 18:54:55 -0700

    refactor: remove example

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,6 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import Meta from "./Meta";
-import styled from "styled-components";
-
-const MyButton = styled.button`
-  background: red;
-  font-size: ${props => (props.huge ? "100px" : "50px")};
-  span {
-    font-size: 100px;
-  }
-`;
 
 export default class Page extends Component {
   render() {
@@ -17,10 +8,6 @@ export default class Page extends Component {
       <div>
         <Meta />
         <Header />
-        <MyButton huge>
-          Click Me
-          <span>poo</span>
-        </MyButton>
         {this.props.children}
       </div>
     );
