46aacc4cc368e79dd73207454c4f5955e49c5f92
Will Harris <will.t.harris@protonmail.com>
2019-12-10 18:53:29 -0700

    feat: add sample styled-components implementation

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import Meta from "./Meta";
+import styled from "styled-components";
+
+const MyButton = styled.button`
+  background: red;
+  font-size: ${props => (props.huge ? "100px" : "50px")};
+  span {
+    font-size: 100px;
+  }
+`;
 
 export default class Page extends Component {
   render() {
@@ -8,6 +17,10 @@ export default class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton huge>
+          Click Me
+          <span>poo</span>
+        </MyButton>
         {this.props.children}
       </div>
     );
