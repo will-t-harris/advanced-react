824e7e8ac7367e54e45827082764031364239916
Will Harris <will.t.harris@protonmail.com>
2019-12-12 18:52:47 -0700

    style: add basic styles to header & page

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import Meta from "./Meta";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 
+const theme = {
+  red: "#ff0000",
+  black: "#393939",
+  grey: "#3a3a3a",
+  lightgrey: "#e1e1e1",
+  offWhite: "#ededed",
+  maxWidth: "1000px",
+  mediaMaxWidth: "1300px",
+  bs: "0 12px 24px 0 rgba(0,0,0,0.09)"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+  background: ${props => props.theme.red};
+`;
 export default class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
